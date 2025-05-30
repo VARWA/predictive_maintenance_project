e18ee1a944f7a9c6a22b991d01f6cdbef50db951
varwa <bogdanlupanov@yandex.ru>
2025-05-30 12:06:32 +0300

    initial commin

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from analysis_and_model import analysis_and_model_page
+from presentation import presentation_page
+
+# Настройка навигации
+pages = {
+    "Анализ": analysis_and_model_page,
+    "Презентация": presentation_page
+}
+
+selected_page = st.sidebar.selectbox("Навигация", list(pages.keys()))
+pages[selected_page]()
